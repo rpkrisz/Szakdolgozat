bb3d05de7eb8e126e1a6cf047229dbf1b59a219e
Réthey-Prikkel <rp.krisz123@gmail.com>
2025-02-16 12:34:46 +0100

    Init commit

---
+++ resources/js/Components/ApplicationLogo.tsx
@@ -0,0 +1,5 @@
+import {SVGAttributes} from "react";
+
+export default function ApplicationLogo(props: SVGAttributes<SVGElement>) {
+  return <img src="assets/mobile-logo.png" alt="Logo" className="size-14 mr-4" />;
+}
